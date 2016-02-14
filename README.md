091a2a58966828888888fd1ffd5944ba656c8959
Raphael Savina <raphael@savina.net>
2016-02-14 13:00:06 +0100

    P6 Udacity Feed Reader Testing

---
+++ README.md
@@ -0,0 +1,29 @@
+# Overview
+
+This project is a web-based application that reads RSS feeds. It uses [Jasmine](http://jasmine.github.io/) for testing.
+
+# How-To:
+
+### Run:
+Open ```index.html``` in your browser. You can also use the command ```python -m SimpleHTTPServer``` in ```index.html``` directory and you should then see:
+```
+Serving HTTP on 0.0.0.0 port 8000 ...
+```
+It means the server is up and running on your computer on port 8000. You can type http://localhost:8000/ into the address bar of a web browser to see the page.
+
+## Run this application
+
+The page will load and run all the test, they would be displayed at the bottom and all tests would appears green.
+
+## Timeout Error
+
+If any of the tests with an async callback are failling with the following error:
+```
+Error: Timeout - Async callback was not invoked within timeout specified by jasmine.DEFAULT_TIMEOUT_INTERVAL.
+```
+You will need then to change the timeout value (value_in_miliseconds) in ```jasmine/spec/feedreader.js``` line 16:
+```
+jasmine.DEFAULT_TIMEOUT_INTERVAL = value_in_miliseconds;
+```
+
+It is set to 20 seconds to give enough time to Google Feed readers API but with some feeds, this might not be enough.
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,146 @@
+/* feedreader.js
+ *
+ * This is the spec file that Jasmine will read and contains
+ * all of the tests that will be run against your application.
+ */
+
+/* We're placing all of our tests within the $() function,
+ * since some of these tests may require DOM elements. We want
+ * to ensure they don't run until the DOM is ready.
+ */
+$(function() {
+    /* Some Ajax calls takes more than the deault 5s so
+     * we need to give Jasmine more time: 20 seconds.
+     */
+    var originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+
+    /* This is our first test suite - a test suite just contains
+     * a related set of tests. This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
+     */
+    describe('RSS Feeds', function() {
+        /* This is our first test - it tests to make sure that the
+         * allFeeds variable has been defined and that it is not
+         * empty.
+         */
+        it('are defined', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+
+        /* Loop through each feed in the allFeeds for tests:
+         *  test_url() and test_name()
+         */
+        for (var x = 0; x < allFeeds.length; x++) {
+            test_url(x);
+            test_name(x);
+        };
+
+        /* Test that feed url is defined
+         * and that the URL is not empty.
+         */
+        function test_url(feedID) {
+            it('Feed ' + allFeeds[feedID].name + ' has an URL that is defined', function() {
+                expect(allFeeds[feedID].url).toBeDefined();
+                expect(allFeeds[feedID].url).not.toBe(0);
+            });
+        };
+
+        /* Test that feed name is defined
+         * and that the name is not empty.
+         */
+        function test_name(feedID) {
+            it('Feed ' + allFeeds[feedID].name + ' has a name that is defined', function() {
+                expect(allFeeds[feedID].name).toBeDefined();
+                expect(allFeeds[feedID].name).not.toBe(0);
+            });
+        };
+
+    });
+
+    /* Test suite for the menu */
+    describe('The menu', function() {
+
+        /* Test that ensures the menu element is hidden by default.
+         * Using jasmine.jquery to help (https://github.com/velesin/jasmine-jquery)
+         */
+        it('is hidden by default', function() {
+            expect($('body')).toHaveClass('menu-hidden');
+        });
+
+        /* Test that ensures the menu changes visibility
+         * when the menu icon is clicked. This has two expectations:
+         * does the menu display when clicked and does it hide when clicked again.
+         */
+        it('click display/hide menu', function() {
+            $('.menu-icon-link').click();
+            expect($('body')).not.toHaveClass('menu-hidden');
+            $('.menu-icon-link').click();
+            expect($('body')).toHaveClass('menu-hidden');
+        });
+
+    });
+
+    /* Test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+
+        /* Test that ensures when the loadFeed function is called
+         * and completed its work, there is at least a single .entry
+         * element within the .feed container. loadFeed() is asynchronous
+         * so it uses Jasmine's beforeEach and asynchronous done() function.
+         */
+
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
+
+        it('loadFeed() is working', function() {
+            expect($('.feed')).not.toBeEmpty();
+            expect($('.entry')).toExist();
+        });
+
+    });
+
+    /* Test suite named "New Feed Selection" */
+
+    describe('New Feed Selection', function() {
+
+        /* Set feed ID as variable for easy change.
+         * Can also be set to 0 for fail
+         */
+        var n = 1;
+        var oldContent;
+        var newContent;
+
+        /* Test that ensures when a new feed is loaded
+         * by loadFeed() that the content actually changes.
+         * loadFeed() is asynchronous, so it uses Jasmine's beforeEach
+         * and asynchronous done() function.
+         */
+        beforeEach(function(done) {
+            /* Keep the content already on the page to compare
+             * with new one
+             */
+            oldContent = $('.feed').text();
+
+            loadFeed(n, function() {
+                /* Get new content on the page to compare*/
+                newContent = $('.feed').text();
+                done();
+            });
+
+        });
+
+        it('a new feed is loaded', function() {
+            expect(newContent).not.toBe(oldContent);
+        });
+
+        afterEach(function() {
+            /* reset Jasmine's timeout to default value */
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+        });
+
+    });
+
+}());
